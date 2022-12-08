66e9d91c4e3582d853b16878de610ac1cad15384
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2022-12-08 20:36:28 +0530

    Create cerateobj.py

---
+++ OOP/cerateobj.py
@@ -0,0 +1,10 @@
+# class abc:
+#     def __init__(self,a) -> None:
+#         self.a = a
+#     def printdata(self):
+#         print(self.a)
+
+# class b:
+#     test = abc(200000000000)
+#     test.printdata()
+# obj = b()
